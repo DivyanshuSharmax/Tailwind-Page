997ee31975827bc874750a9d04e95d72a22ac300
Divyanshu <divinnit@gmail.com>
2024-10-16 17:19:25 +0530

    for the main branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        '4xl': '2.5rem', // Example custom size
+        '5xl': '3rem',
+      },
+      clipPath:{
+        'trap': 'polygon(25% 0%, 75% 0%, 100% 100%, 0% 100%)',
+      }
+
+    },
   },
   plugins: [],
 }
